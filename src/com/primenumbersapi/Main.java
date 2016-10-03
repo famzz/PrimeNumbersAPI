350c8b476740e25165c6946785dd063360a7c76d
UncookedYogurt <jessewilson2000@gmail.com>
2016-10-03 13:00:09 +0100

    Removed GUI code w/ name changes
    Jesse Removed the unnecessary GUI code and he changed the names on some of the methods so they weren't so generic i.e. "Main"

--- src/com/primenumbersapi/Main.java
+++ src/com/primenumbersapi/Main.java
@@ -1,23 +1,14 @@
 package com.primenumbersapi;
 
-import com.primenumbersapi.gui.View;
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        View thisView = new View();
-        thisView.createView(primaryStage);
-
+public class PrimeNumbersAPI {
+    
+    private PrimeNumbersAPI() {
+        
     }
-
-    public static void main(String[] args) {
+    
+    public static PrimeNumbersAPI instantiate() {
         // Example code showing how to initialise the database manager.
         //String path = new File("src\\resources\\primes.db").getAbsolutePath();
         //DatabaseManager databaseManager = new DatabaseManager("jdbc:sqlite:" + path, "primes");
-        launch(args);
     }
 }
