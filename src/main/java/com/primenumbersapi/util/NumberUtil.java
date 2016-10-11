dc9f389fb2063881a465c6273b814c062617f2c8
Abdul Al-Fahim <abdul.al-fahim@live.com>
2016-10-11 22:09:03 +0100

    Added a subtraction method based on two's complement (read more here: https://www.mathsisfun.com/numbers/subtraction-by-addition.html). Documented the addition and subtraction methods to clearly show how they work (the subtraction method can be a bit confusing). Need to add support for negative answers in future releases.

--- src/main/java/com/primenumbersapi/util/NumberUtil.java
+++ src/main/java/com/primenumbersapi/util/NumberUtil.java
@@ -4,6 +4,13 @@ import com.primenumbersapi.exception.InvalidDigitException;
 
 public class NumberUtil {
 
+    /**
+     * A method that adds two strings together, inspired by column method.
+     * @param number1 The first string to add. Must be an integer.
+     * @param number2 The second string to add. Must be an integer.
+     * @return The result of the sum, as a string.
+     * @throws InvalidDigitException If any of the arguments suppied are not integers.
+     */
     public static String add(String number1, String number2) throws InvalidDigitException {
         isInteger(number1);
         isInteger(number2);
@@ -45,6 +52,73 @@ public class NumberUtil {
         }
 
         return number;
+    }
+
+    /**
+     * A method for subtracting two strings based on the two's complement system that computers use for subtraction.
+     * This method follows the non-commutative property of subtraction, so the position of the arguments supplied
+     * matters! Currently, this method cannot deal with negative answers, so if it is told to subtract a smaller number
+     * from a larger one then it will return an empty string.
+     * @param number1 The number to subtract from. Must be an integer.
+     * @param number2 The amount to subtract. Cannot be greater than number1. Must be an integer.
+     * @return The answer of the subtraction, as a string.
+     * @throws InvalidDigitException If non integer strings are supplied as arguments.
+     */
+    public static String subtract(String number1, String number2) throws InvalidDigitException {
+        isInteger(number1);
+        isInteger(number2);
+
+        String answer;
+        String complement = "";
+        int prevDigit = 0;
+        boolean noMoreZeroes = false;
+        boolean doneFirstDigit = false;
+        int numberOfZeroes = 0;
+
+        for (int i = 1; i <= number2.length(); i++) {
+            int digit = Character.getNumericValue(number2.charAt(number2.length() - i));
+
+            if (digit == 0 && prevDigit == 0 && !noMoreZeroes) {
+                numberOfZeroes++;
+                // If the digit we are subtracting is 0 and the previous digit was 0 and we haven't encountered a non
+                // zero integer yet, then we don't process the digit.
+                continue;
+            }
+
+            // Set to true is we encounter our fist non zero integer.
+            noMoreZeroes = true;
+
+            // We may not have subtracted from any digits yet if number2 ends in zeroes.
+            if (!doneFirstDigit) {
+                digit = 10 - digit;
+                doneFirstDigit = true;
+            } else {
+                digit = 9 - digit;
+            }
+
+            prevDigit = digit;
+
+            complement = digit + complement;
+        }
+
+        for (int i = 0; i < numberOfZeroes; i++) {
+            // Pad the complement with zeroes if number2 ends in zeroes.
+            complement = complement + "0";
+        }
+
+        if (complement.length() < number1.length()) {
+            while (complement.length() < number1.length()) {
+                // Pad the complement with nines at the beginning if it is shorter than number1.
+                complement = "9" + complement;
+            }
+        }
+
+        answer = add(number1, complement);
+
+        // Remove the leading character from the final answer.
+        answer = answer.substring(1);
+
+        return answer;
     }
 
     private static void isInteger(String number) throws InvalidDigitException {
