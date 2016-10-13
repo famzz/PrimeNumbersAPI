22b219dc3daf4a55ee06ca59a369a9e46507ddf6
Abdul Al-Fahim <abdul.al-fahim@live.com>
2016-10-13 21:47:20 +0100

    Add support for negatives to nearly every method in NumberUtil except NumberUtil::multiply. Also, NumberUtil::multiply is very inefficient (2 squared takes an average of 0 seconds to compute, whereas 1 million squared takes an average of 4.1 seconds to compute!) So we will need to change to a more efficient multiplication method in the future.

--- src/main/java/com/primenumbersapi/util/NumberUtil.java
+++ src/main/java/com/primenumbersapi/util/NumberUtil.java
@@ -5,7 +5,8 @@ import com.primenumbersapi.exception.InvalidDigitException;
 public class NumberUtil {
 
     /**
-     * A method that adds two strings together, inspired by column method.
+     * A method that adds two strings together, inspired by column method. Supports negatives (add a - (hyphen) sign to
+     * the addend that you wish to make negative).
      * @param addend1 The first string to add. Must be an integer.
      * @param addend2 The second string to add. Must be an integer.
      * @return The result of the sum, as a string.
@@ -16,6 +17,32 @@ public class NumberUtil {
         isInteger(addend2);
 
         String number = "";
+
+        boolean isAddend1Negative = false;
+        boolean isAddend2Negative = false;
+
+        if ("-".equals(String.valueOf(addend1.charAt(0)))) {
+            isAddend1Negative = true;
+        }
+
+        if ("-".equals(String.valueOf(addend2.charAt(0)))) {
+            isAddend2Negative = true;
+        }
+
+        if (!isAddend1Negative && isAddend2Negative) {
+            addend2 = addend2.substring(1);
+            return subtract(addend1, addend2);
+        }
+
+        if (isAddend1Negative && !isAddend2Negative) {
+            addend1 = addend1.substring(1);
+            return subtract(addend2, addend1);
+        }
+
+        if (isAddend1Negative) {
+            addend1 = addend1.substring(1);
+            addend2 = addend2.substring(1);
+        }
 
         if (addend2.length() > addend1.length()) {
             while (addend2.length() > addend1.length()) {
@@ -51,13 +78,17 @@ public class NumberUtil {
             number = carry + number;
         }
 
+        if (isAddend1Negative) {
+            number = "-" + number;
+        }
+
         return number;
     }
 
     /**
      * A method for subtracting two strings based on the two's complement system that computers use for subtraction.
      * This method follows the non-commutative property of subtraction, so the position of the arguments supplied
-     * matters!
+     * matters! Supports negatives (add a - (hyphen) sign to the number that you wish to make negative).
      * @param minuere The number to subtract from. Must be an integer.
      * @param subtrahend The amount to subtract. Must be an integer.
      * @return The answer of the subtraction, as a string.
@@ -67,8 +98,33 @@ public class NumberUtil {
         isInteger(minuere);
         isInteger(subtrahend);
 
-        // If the minuere is less than the subtrahend then our answer will be negative.
         boolean isLess = isLess(minuere, subtrahend);
+
+        boolean isMinuereNegative = false;
+        boolean isSubtrahendNegative = false;
+
+        if ("-".equals(String.valueOf(minuere.charAt(0)))) {
+            isMinuereNegative = true;
+        }
+
+        if ("-".equals(String.valueOf(subtrahend.charAt(0)))) {
+            isSubtrahendNegative = true;
+        }
+
+        if (!isMinuereNegative && isSubtrahendNegative) {
+            subtrahend = subtrahend.substring(1);
+            return add(minuere, subtrahend);
+        }
+
+        if (isMinuereNegative && !isSubtrahendNegative) {
+            return "-" + add(minuere.substring(1), subtrahend);
+        }
+
+        if (isMinuereNegative) {
+            return add(subtrahend.substring(1), minuere);
+        }
+
+        // If the minuere is less than the subtrahend then our answer will be negative.
         if (isLess) {
             // Swap the minuere and the subtrahend and carry out the absolute value of the subtraction.
             String tempMinuere = minuere;
@@ -133,7 +189,47 @@ public class NumberUtil {
     }
 
     /**
-     * Compare two strings and determine if one is greater than the other.
+     * A "place holder" method that does multiplication using repeated addition. This method is very inefficient when
+     * handling large numbers, so it will be replaced by a more efficient multiplication method in the future.
+     * @param multiplicand The first number to multiply. Must be an integer.
+     * @param multiplier The second number to multiply. Must be an integer.
+     * @return The answer of the multiplication as a string.
+     * @throws InvalidDigitException If non integer strings are supplied as arguments.
+     */
+    public static String multiply(String multiplicand, String multiplier) throws InvalidDigitException {
+        isInteger(multiplicand);
+        isInteger(multiplier);
+
+        if (multiplier.equals("0")) {
+            return "0";
+        } else if (multiplier.equals("1")) {
+            return multiplicand;
+        }
+
+        if (isGreater(multiplier, multiplicand)) {
+            String tempMultiplier = multiplier;
+
+            multiplier = multiplicand;
+            multiplicand = tempMultiplier;
+        }
+
+        String product = "";
+        while (isGreater(multiplier, "1")) {
+            if (product.length() == 0) {
+                product = add(multiplicand, multiplicand);
+            } else {
+                product = add(product, multiplicand);
+            }
+
+            multiplier = subtract(multiplier, "1");
+        }
+
+        return product;
+    }
+
+    /**
+     * Compare two strings and determine if one is greater than the other. Supports negatives (add a - (hyphen) sign to
+     * the number that you wish to make negative).
      * @param number1 The first number to compare. Must be an integer.
      * @param number2 The second number to compare. Must be an integer.
      * @return True or false, depending on the result of the comparison.
@@ -143,13 +239,33 @@ public class NumberUtil {
         isInteger(number1);
         isInteger(number2);
 
-        if (Character.getNumericValue(number1.charAt(0)) == 0) {
+        boolean isNumber1Negative = false;
+        boolean isNumber2Negative = false;
+
+        if ("-".equals(String.valueOf(number1.charAt(0)))) {
+            isNumber1Negative = true;
+        }
+
+        if ("-".equals(String.valueOf(number2.charAt(0)))) {
+            isNumber2Negative = true;
+        }
+
+        // Remove the negative signs and process them normally.
+        if (isNumber1Negative) {
+            number1 = number1.substring(1);
+        }
+
+        if (isNumber2Negative) {
+            number2 = number2.substring(1);
+        }
+
+        if (Character.getNumericValue(number1.charAt(0)) == 0 && number1.length() != 1) {
             while (Character.getNumericValue(number1.charAt(0)) == 0) {
                 number1 = number1.substring(1);
             }
         }
 
-        if (Character.getNumericValue(number2.charAt(0)) == 0) {
+        if (Character.getNumericValue(number2.charAt(0)) == 0 && number2.length() != 1) {
             while (Character.getNumericValue(number2.charAt(0)) == 0) {
                 number2 = number2.substring(1);
             }
@@ -164,10 +280,19 @@ public class NumberUtil {
                 int digit1 = Character.getNumericValue(number1.charAt(i));
                 int digit2 = Character.getNumericValue(number2.charAt(i));
 
+                // I have purposely chosen not to simplify the following if statements to preserve readability!
                 if (digit1 > digit2) {
-                    return true;
+                    if (isNumber1Negative && !isNumber2Negative) {
+                        return false;
+                    } else if (!isNumber1Negative && isNumber2Negative) {
+                        return true;
+                    } else return !isNumber1Negative;
                 } else if (digit1 < digit2) {
-                    return false;
+                    if (isNumber1Negative && !isNumber2Negative) {
+                        return false;
+                    } else if (!isNumber1Negative && isNumber2Negative) {
+                        return true;
+                    } else return isNumber1Negative;
                 }
             }
         }
@@ -176,7 +301,8 @@ public class NumberUtil {
     }
 
     /**
-     * Compare two strings and determine whether one is less than the other.
+     * Compare two strings and determine whether one is less than the other. Supports negatives (add a - (hyphen) sign
+     * to the number that you wish to make negative).
      * @param number1 The first number to compare. Must be an integer.
      * @param number2 The second number to compare. Must be an integer.
      * @return True or false, depending on the result of the comparison.
@@ -187,23 +313,29 @@ public class NumberUtil {
     }
 
     /**
-     * Compare two strings and determine whether one is equal to another.
+     * Compare two strings and determine whether one is equal to another. Supports negatives (add a - (hyphen) sign to
+     * the number that you wish to make negative).
      * @param number1 The first number to compare. Must be an integer.
      * @param number2 The second number to compare. Must be an integer.
-     * @return
+     * @return True or false, depending on the result of the comparison.
      * @throws InvalidDigitException If non integer strings are supplied as arguments.
      */
     public static boolean isEqual(String number1, String number2) throws InvalidDigitException {
         isInteger(number1);
         isInteger(number2);
 
-        if (Character.getNumericValue(number1.charAt(0)) == 0) {
+        if ("-".equals(String.valueOf(number1.charAt(0))) && "-".equals(String.valueOf(number2.charAt(0)))) {
+            number1 = number1.substring(1);
+            number2 = number2.substring(1);
+        }
+
+        if (Character.getNumericValue(number1.charAt(0)) == 0 && number1.length() != 1) {
             while (Character.getNumericValue(number1.charAt(0)) == 0) {
                 number1 = number1.substring(1);
             }
         }
 
-        if (Character.getNumericValue(number2.charAt(0)) == 0) {
+        if (Character.getNumericValue(number2.charAt(0)) == 0 && number2.length() != 1) {
             while (Character.getNumericValue(number2.charAt(0)) == 0) {
                 number2 = number2.substring(1);
             }
@@ -234,7 +366,7 @@ public class NumberUtil {
     }
 
     private static boolean isDigit(char c) {
-        return (c >= '0' && c <= '9');
+        return ((c >= '0' && c <= '9') || ("-").equals(String.valueOf(c)));
     }
 
 }
